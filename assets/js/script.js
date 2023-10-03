0b19709b2c4a1cee312fedcfbb31f1f398bead87
Thomas Macé <57435831+ScalaAdCoelum@users.noreply.github.com>
2023-10-03 17:52:57 +0200

    Add music

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const taskInput = document.getElementById('task');
     const addTaskButton = document.getElementById('addTask');
     const taskList = document.getElementById('taskList');
+    const themeSelector = document.getElementById('theme');
     const pinImage = document.getElementById('pin-image');
-    let isDragging = false;
-    let currentTask = null;
-    let offsetX, offsetY;
 
     function setTheme(theme) {
         document.body.className = `theme-${theme}`;
@@ -24,21 +22,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    const themeSelector = document.getElementById('theme');
     themeSelector.addEventListener('change', function () {
         const selectedTheme = themeSelector.value;
         setTheme(selectedTheme);
     });
 
     setTheme('default');
-
-    function deleteTask(listItem) {
-        listItem.remove();
-    }
-
-    function toggleCompleted(listItem) {
-        listItem.classList.toggle('completed');
-    }
 
     addTaskButton.addEventListener('click', function () {
         const taskText = taskInput.value.trim();
@@ -49,48 +38,36 @@ document.addEventListener('DOMContentLoaded', function () {
             listItem.innerHTML = `
                 <img src="assets/images/favicon.png" class="task-icon">
                 <span>${taskText}</span>
-                <button class="delete"><i class="ri-delete-bin-line"></i></button>
-                <button class="complete"><i class="ri-check-double-line"></i></button>
+                <button class="complete">Complete</button>
             `;
-
-            listItem.draggable = true;
-
-            listItem.addEventListener('dragstart', function (e) {
-                isDragging = true;
-                currentTask = e.target;
-                offsetX = e.clientX - currentTask.offsetLeft;
-                offsetY = e.clientY - currentTask.offsetTop;
-            });
-
-            listItem.addEventListener('dragend', function () {
-                isDragging = false;
-                currentTask = null;
-            });
-
-            listItem.addEventListener('dragover', function (e) {
-                e.preventDefault();
-            });
-
-            listItem.addEventListener('drop', function (e) {
-                if (currentTask !== null) {
-                    taskList.insertBefore(currentTask, listItem);
-                }
-            });
 
             taskList.appendChild(listItem);
             taskInput.value = '';
 
-            // Add event listener to the delete button
-            const deleteButton = listItem.querySelector('button.delete');
-            deleteButton.addEventListener('click', function () {
-                deleteTask(listItem);
-            });
-
-            // Add event listener to the complete button
             const completeButton = listItem.querySelector('button.complete');
             completeButton.addEventListener('click', function () {
-                toggleCompleted(listItem);
+                listItem.classList.toggle('completed');
             });
         }
+    });
+
+    const musicToggle = document.getElementById('music-toggle');
+    const musicOffButton = document.getElementById('music-off');
+    const music = document.getElementById('music');
+
+    // Start with music off
+    music.pause();
+    musicOffButton.style.display = 'none';
+
+    musicToggle.addEventListener('click', () => {
+        music.play();
+        musicToggle.style.display = 'none';
+        musicOffButton.style.display = 'inline-block';
+    });
+
+    musicOffButton.addEventListener('click', () => {
+        music.pause();
+        musicToggle.style.display = 'inline-block';
+        musicOffButton.style.display = 'none';
     });
 });
